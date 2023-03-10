72a0b6e7a27e0a9bd91bb243929aaf48ff43557d
VladBlow <vladimir.tsibizow@gmail.com>
2023-03-10 20:24:10 +0300

    Added images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,162 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const useImage = (id) => {
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    const [image, setImage] = useState(null)
+
+    useEffect(() => {
+        const fetchImage = async () => {
+            try {
+                const response = await import(`./images/${id}.jpeg`) // change relative path to suit your needs
+                setImage(response.default)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchImage()
+    }, [id])
+
+    return {
+        loading,
+        error,
+        image,
+    }
+}
+
+const Image = ({ id, alt, className, ...rest }) => {
+  const { loading, error, image } = useImage(id)
+
+  if (error) return <span>{alt}</span>
+
+  return (
+      <>
+          {loading ? (
+              <span>loading</span>
+          ) : (
+              <img
+                  className={`Image${
+                      className
+                          ? className.padStart(className.length + 1)
+                          : ''
+                  }`}
+                  src={image}
+                  alt={alt}
+                  {...rest}
+              />
+          )}
+      </>
+  )
+}
+
+async function getData() {
+  // https://stackoverflow.com/questions/31765773/converting-google-visualization-query-result-into-javascript-array
+  // https://developers.google.com/chart/interactive/docs/dev/implementing_data_source#responseformat
+
+  const spreadsheetId = "1LHSC41Hr_5PjfhAhrTvhlWNToHXgcQI6",
+    response = await fetch(
+      `https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:json`
+    ),
+    result = await response.text(),
+    json = JSON.parse(
+      result.replace(
+        /.*google.visualization.Query.setResponse\({(.*?)}\);?/s,
+        "{$1}"
+      )
+    );
+
+  // console.log(json);
+
+  // `table.cols` element contains headings
+  // we will use them to build our data array
+  const headings = json.table.cols.map((item) => item.id);
+
+  console.log(json, headings);
+
+  // console.log(headings);
+
+  // data of each row is associated to the headings
+  let data = json.table.rows.map((item) => {
+    // console.log(item);
+    let row = {};
+    item.c.forEach((cell, idx) => {
+      // console.log(cell)
+      row[headings[idx]] = cell?.v;
+    });
+    // console.log(row)
+    return row;
+  });
+
+  data = data.filter((o) => o.A && o.B);
+
+  // filtering and sorting
+  // data = data.filter(item => item.Publish === true);
+  // data.sort((a, b) => a.CategoryOrder > b.CategoryOrder);
+
+  // console.log(data);
+
+  /*
+    Fields:
+    -------------------
+    Category
+    Name
+    Description
+    Price
+    Publish
+    CategoryOrder
+    CategoryTranslation
+  */
+
+  // aggregating data by category
+  // data = [...new Set(data.map(item => item.CategoryOrder))].map(categoryIndex => {
+  //   return data.filter(item => item.CategoryOrder === categoryIndex);
+  // });
+
+  return data;
+}
+
+export default function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getData();
+      setData(data);
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data.length ? "Список пластинок" : "Загрузка..."}</h1>
+      <input placeholder="Поиск" value={search} onChange={e => setSearch(e.target.value)} />
+      <div className="flex">
+        {data.filter(i => i.C?.toLowerCase().includes(search?.toLowerCase()) || i.B?.toLowerCase().includes(search?.toLowerCase())).map((item) => (
+          <div>
+            <Image id={item.A} />
+            <h3>{item.C} – {item.B}</h3>
+            {/* <div>Альбом: <b> </b></div>
+            <div>Исполнитель: <b> </b></div> */}
+            {/* <div>Жанр: {item.D}</div> */}
+            <br />
+            <br />
+            <br />
+            <br />
+            <div>Цена: {item.E} ₽</div>
+            {/* <div>Состояние: {item.H}</div> */}
+            {/* <div>Год: {item.K}</div> */}
+            {/* <div>Лейбл: {item.M}</div> */}
+            {/* <div>Страна: {item.N}</div> */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
